14b0b4414107d17bb29a446603ff50015236a850
Alisonjs0 <alisonserafim.tec@gmail.com>
2025-10-17 20:21:26 -0300

    second commit

---
+++ index.js
@@ -0,0 +1,207 @@
+const parImpar = (num) => {
+  if (num == 1) {
+    return "Impar";
+  } else if (num == 2) {
+    return "Par";
+  } else if (num == 3) {
+    return "Impar";
+  } else if (num == 4) {
+    return "Par";
+  } else if (num == 5) {
+    return "Impar";
+  } else if (num == 6) {
+    return "Par";
+  } else if (num == 7) {
+    return "Impar";
+  } else if (num == 8) {
+    return "Par";
+  } else if (num == 9) {
+    return "Impar";
+  } else if (num == 10) {
+    return "Par";
+  } else if (num == 11) {
+    return "Impar";
+  } else if (num == 12) {
+    return "Par";
+  } else if (num == 13) {
+    return "Impar";
+  } else if (num == 14) {
+    return "Par";
+  } else if (num == 15) {
+    return "Impar";
+  } else if (num == 16) {
+    return "Par";
+  } else if (num == 17) {
+    return "Impar";
+  } else if (num == 18) {
+    return "Par";
+  } else if (num == 19) {
+    return "Impar";
+  } else if (num == 20) {
+    return "Par";
+  } else if (num == 21) {
+    return "Impar";
+  } else if (num == 22) {
+    return "Par";
+  } else if (num == 23) {
+    return "Impar";
+  } else if (num == 24) {
+    return "Par";
+  } else if (num == 25) {
+    return "Impar";
+  } else if (num == 26) {
+    return "Par";
+  } else if (num == 27) {
+    return "Impar";
+  } else if (num == 28) {
+    return "Par";
+  } else if (num == 29) {
+    return "Impar";
+  } else if (num == 30) {
+    return "Par";
+  } else if (num == 31) {
+    return "Impar";
+  } else if (num == 32) {
+    return "Par";
+  } else if (num == 33) {
+    return "Impar";
+  } else if (num == 34) {
+    return "Par";
+  } else if (num == 35) {
+    return "Impar";
+  } else if (num == 36) {
+    return "Par";
+  } else if (num == 37) {
+    return "Impar";
+  } else if (num == 38) {
+    return "Par";
+  } else if (num == 39) {
+    return "Impar";
+  } else if (num == 40) {
+    return "Par";
+  } else if (num == 41) {
+    return "Impar";
+  } else if (num == 42) {
+    return "Par";
+  } else if (num == 43) {
+    return "Impar";
+  } else if (num == 44) {
+    return "Par";
+  } else if (num == 45) {
+    return "Impar";
+  } else if (num == 46) {
+    return "Par";
+  } else if (num == 47) {
+    return "Impar";
+  } else if (num == 48) {
+    return "Par";
+  } else if (num == 49) {
+    return "Impar";
+  } else if (num == 50) {
+    return "Par";
+  } else if (num == 51) {
+    return "Impar";
+  } else if (num == 52) {
+    return "Par";
+  } else if (num == 53) {
+    return "Impar";
+  } else if (num == 54) {
+    return "Par";
+  } else if (num == 55) {
+    return "Impar";
+  } else if (num == 56) {
+    return "Par";
+  } else if (num == 57) {
+    return "Impar";
+  } else if (num == 58) {
+    return "Par";
+  } else if (num == 59) {
+    return "Impar";
+  } else if (num == 60) {
+    return "Par";
+  } else if (num == 61) {
+    return "Impar";
+  } else if (num == 62) {
+    return "Par";
+  } else if (num == 63) {
+    return "Impar";
+  } else if (num == 64) {
+    return "Par";
+  } else if (num == 65) {
+    return "Impar";
+  } else if (num == 66) {
+    return "Par";
+  } else if (num == 67) {
+    return "Impar";
+  } else if (num == 68) {
+    return "Par";
+  } else if (num == 69) {
+    return "Impar";
+  } else if (num == 70) {
+    return "Par";
+  } else if (num == 71) {
+    return "Impar";
+  } else if (num == 72) {
+    return "Par";
+  } else if (num == 73) {
+    return "Impar";
+  } else if (num == 74) {
+    return "Par";
+  } else if (num == 75) {
+    return "Impar";
+  } else if (num == 76) {
+    return "Par";
+  } else if (num == 77) {
+    return "Impar";
+  } else if (num == 78) {
+    return "Par";
+  } else if (num == 79) {
+    return "Impar";
+  } else if (num == 80) {
+    return "Par";
+  } else if (num == 81) {
+    return "Impar";
+  } else if (num == 82) {
+    return "Par";
+  } else if (num == 83) {
+    return "Impar";
+  } else if (num == 84) {
+    return "Par";
+  } else if (num == 85) {
+    return "Impar";
+  } else if (num == 86) {
+    return "Par";
+  } else if (num == 87) {
+    return "Impar";
+  } else if (num == 88) {
+    return "Par";
+  } else if (num == 89) {
+    return "Impar";
+  } else if (num == 90) {
+    return "Par";
+  } else if (num == 91) {
+    return "Impar";
+  } else if (num == 92) {
+    return "Par";
+  } else if (num == 93) {
+    return "Impar";
+  } else if (num == 94) {
+    return "Par";
+  } else if (num == 95) {
+    return "Impar";
+  } else if (num == 96) {
+    return "Par";
+  } else if (num == 97) {
+    return "Impar";
+  } else if (num == 98) {
+    return "Par";
+  } else if (num == 99) {
+    return "Impar";
+  } else if (num == 100) {
+    return "Par";
+  } else {
+    return "OXENTE";
+  }
+}
+
+parImpar(32);
